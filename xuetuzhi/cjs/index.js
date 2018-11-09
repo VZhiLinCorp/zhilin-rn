1fd739512aaa08f54364ffe47169b6cce78109f0
帆 <lee@fandeMacBook-Air.local>
2018-11-09 18:19:51 +0800

    cjs

--- xuetuzhi/cjs/index.js
+++ xuetuzhi/cjs/index.js
@@ -12,11 +12,14 @@ Object.defineProperty(exports, 'type', {
     return 'main';
   }
 });
+
+var _index = require('./ProgressCircle');
 Object.defineProperty(exports, 'ProgressCircle', {
   enumerable: true,
   get: function get() {
-    return 'ProgressCircle';
+    return _interopRequireDefault(_index)['default'];;
   }
 });
+
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { 'default': obj }; }
