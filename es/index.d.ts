fdbc1b407e303b4fb8cfbb8a17a90b3deecfaba9
帆 <lee@fandeMacBook-Air.local>
2018-11-09 15:18:19 +0800

    小改动

--- es/index.d.ts
+++ es/index.d.ts
@@ -1,4 +1,3 @@
 
-export { default as Button } from './button/index.native';
 
 export const type = 'module'
--- es/index.js
+++ es/index.js
@@ -1,3 +1,2 @@
 
-export { default as Button } from './button/index.native';
 export const type = 'module'
