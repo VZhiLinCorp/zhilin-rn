535a5ede030e17ccb04c0c8fd8ff0cb2cf23e32f
帆 <lee@fandeMacBook-Air.local>
2018-11-10 12:57:14 +0800

    入口

--- xuetuzhi/esm/index.d.ts
+++ xuetuzhi/esm/index.d.ts
@@ -2,4 +2,5 @@
 export {default as ProgressCircle} from './ProgressCircle'
 export {default as PaddingBox} from './PaddingBox'
 export {default as Touch} from './Touch'
+export {default as FileItem} from './FileItem'
 
