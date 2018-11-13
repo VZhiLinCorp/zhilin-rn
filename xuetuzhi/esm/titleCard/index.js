a8bf34a1568dd4d3151d1c89a02135d9f7264b3f
帆 <lee@fandeMacBook-Air.local>
2018-11-13 16:43:13 +0800

    titleCard

--- xuetuzhi/esm/titleCard/index.js
+++ xuetuzhi/esm/titleCard/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { View, Text, Card, CardItem, Right } from 'native-base'
 
-import { bgWhite, colorInfo, bgPrimary, alignItemsC, spaceBtw, row, getFlex, bold, getMorP, fontSizeN, fontSizeSm, ml_sm } from '../../../styles';
+import { bgWhite, colorInfo, bgPrimary, alignItemsC, spaceBtw, row, getFlex, bold, getMorP, fontSizeN, fontSizeSm, ml_sm, getHeight,getBorder } from '../../../styles';
 
 
 export default class TitleCard extends React.Component {
@@ -18,7 +18,7 @@ export default class TitleCard extends React.Component {
         const { title, children, right,renderRight } = this.props
         return (
             <Card style={[bgWhite]}>
-                <CardItem header bordered style={[getMorP(1, 0, 'l')]}>
+                <View bordered style={[getMorP(1, 0, 'l'),getHeight(50),row,alignItemsC,getBorder('b')]}>
                     <View style={[{ width: 3, height: 20, borderTopRightRadius: 5, borderBottomRightRadius: 5 }, bgPrimary]}>
                     </View>
                     <View style={[row, alignItemsC, spaceBtw, getFlex(1), ml_sm]}>
@@ -27,7 +27,7 @@ export default class TitleCard extends React.Component {
                             renderRight?renderRight():<Text style={[fontSizeSm, colorInfo,]}>{right}</Text>
                         }
                     </View>
-                </CardItem>
+                </View>
                 <CardItem header>
 
                     {
