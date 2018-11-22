8a6cad2a237a07cdb64a583fbcaa34d19821d1cc
帆 <lee@fandeMacBook-Air.local>
2018-11-22 11:44:37 +0800

    StatusBadge

--- xuetuzhi/esm/StatusBadge/index.js
+++ xuetuzhi/esm/StatusBadge/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { View, Text } from 'react-native'
 
-import { pp_xs, jc_c, getMorP, getRadius,getStupidBg, colorWhite } from '../../../styles';
+import { pp_xs, jc_c, getMorP, getRadius, getStupidBg, colorWhite } from '../../../styles';
 export default class MyBadge extends React.PureComponent {
   constructor() {
     super(...arguments)
@@ -9,12 +9,10 @@ export default class MyBadge extends React.PureComponent {
 
   render() {
     const { text } = this.props
-    return (
-      <View style={[pp_xs, getStupidBg(text), jc_c, getMorP(1, 2, 'tblr'),getRadius(2)]}>
-        <Text style={[colorWhite]}>
-          {text}
-        </Text>
-      </View>
-    )
+    return !!text && <View style={[pp_xs, getStupidBg(text), jc_c, getMorP(1, 2, 'tblr'), getRadius(2)]}>
+      <Text style={[colorWhite]}>
+        {text}
+      </Text>
+    </View>
   }
 }
