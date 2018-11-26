950dac91d9535665cc502e3589f5c02aaafef065
帆 <lee@fandeMacBook-Air.local>
2018-11-26 16:14:18 +0800

    TitleCard

---
+++ xuetuzhi/esm/TitleCard/index.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import { View, Text, Card, CardItem, Right } from 'native-base'
+
+import { bgWhite, colorInfo, bgPrimary, alignItemsC, spaceBtw, row, getFlex, bold, getMorP, fontSizeN, fontSizeSm, ml_sm, getHeight, mp_sm } from '../../../styles';
+import { getBorder } from '../../../../../src/styles';
+
+
+export default class TitleCard extends React.Component {
+    constructor() {
+        super(...arguments)
+        this.state = {
+
+        }
+    }
+    static defaultProps = {
+        right: ''
+    }
+    render() {
+        const { title, children, right,renderRight } = this.props
+        return (
+            <Card style={[bgWhite]}>
+                <View bordered style={[getMorP(1, 0, 'l'),getHeight(50),row,alignItemsC,getBorder('b')]}>
+                    <View style={[{ width: 3, height: 20, borderTopRightRadius: 5, borderBottomRightRadius: 5 }, bgPrimary]}>
+                    </View>
+                    <View style={[row, alignItemsC, spaceBtw, getFlex(1), mp_sm]}>
+                        <Text style={[bold, fontSizeN, getFlex()]} numberOfLines={1}>{title || '标题为空'}</Text>
+                        {
+                            renderRight?renderRight():<Text style={[fontSizeSm, colorInfo,]}>{right}</Text>
+                        }
+                    </View>
+                </View>
+                <CardItem header>
+
+                    {
+                        React.Children.map(children, c => React.cloneElement(c))
+                    }
+                </CardItem>
+            </Card>
+        )
+    }
+}
