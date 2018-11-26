50fced5141df3dd51b673d1b8c5d4d832da3342f
帆 <lee@fandeMacBook-Air.local>
2018-11-26 18:24:27 +0800

    主题

--- bin/index.js
+++ bin/index.js
@@ -1,2 +1,16 @@
 #!/usr/bin/env node
-console.log('hi')
+var path = require('path')
+var fs = require('fs')
+
+var TAGET = 'theme'
+var root = path.join(__dirname, '../../../')
+var pkg = fs.readFileSync(path.resolve(root, 'package.json')).toString('utf8')
+var pkgThemePath = JSON.parse(pkg)[TAGET]
+var customThemePath = path.resolve(root + pkgThemePath)
+var customTheme = require(customThemePath) || {}
+var themePath = path.resolve(require.resolve('zhilin-rn'), '../../theme.js')
+var theme = require(themePath)
+var result = Object.assign({}, theme, customTheme)
+if (fs.existsSync(themePath)) {
+    fs.writeFileSync(themePath, `module.exports = ${JSON.stringify(result)}`)
+}
