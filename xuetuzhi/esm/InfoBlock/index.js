07c2da1f13cdfca7a27d795c03c044d69cf942f7
Dainel <benpeng.liu@vzhilin.com>
2018-11-30 15:23:00 +0800

    Info

--- xuetuzhi/esm/InfoBlock/index.js
+++ xuetuzhi/esm/InfoBlock/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { View, Text } from 'react-native'
-import { fontSizeXs, row, spaceBtw, mt_xs, bgPrimary, colorWhite, getFontWeight, mt_sm, fontSizeSm } from '../../../styles';
+import LinearGradient from 'react-native-linear-gradient';
+import { fontSizeXs, row, spaceBtw, mt_xs, bgPrimary, colorWhite, getFontWeight, mt_sm, fontSizeSm, getFlex } from '../../../styles';
 
 import { StatusBadge, PaddingBox } from '../index';
 export default class InfoBlock extends React.Component {
@@ -9,25 +10,26 @@ export default class InfoBlock extends React.Component {
 
     }
     render() {
-        const { rows = [], style = [], onPress, bg = bgPrimary, color = colorWhite, textStyle = [] } = this.props;
-        const _textStyle = [color, fontSizeSm, getFontWeight('800'), ...textStyle]
+        const { rows = [], style = [], onPress, bg = bgPrimary, bgColors = ['#66ccff', '#3399ff', '#0066cc'], color = colorWhite, textStyle = [] } = this.props;
+        const _textStyle = [color, fontSizeSm, ...textStyle]
         return (
-            <PaddingBox style={[bg, ...style]} onPress={onPress} >
-
-                {
-                    rows.map((r, index) => {
-                        return (
-                            <View style={[row, spaceBtw, (index !== 0) && mt_sm]}>
-                                {
-                                    r.map(rowItem => {
-                                        return <Text style={_textStyle}>{rowItem}</Text>
-                                    })
-                                }
-                            </View>
-                        )
-                    })
-                }
-            </PaddingBox>
+            <LinearGradient colors={bgColors}  start={{ x: 0, y: 0 }} end={{ x: 1, y: 0 }}>
+                <PaddingBox style={[...style]} onPress={onPress}>
+                    {
+                        rows.map((r, index) => {
+                            return (
+                                <View style={[row, spaceBtw, (index !== 0) && mt_sm]}>
+                                    {
+                                        r.map(rowItem => {
+                                            return <Text style={_textStyle}>{rowItem}</Text>
+                                        })
+                                    }
+                                </View>
+                            )
+                        })
+                    }
+                </PaddingBox>
+            </LinearGradient>
         )
     }
 
