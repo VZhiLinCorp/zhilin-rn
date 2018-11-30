9599002d000c83026b13a404c08cda143681c0b6
帆 <lee@fandeMacBook-Air.local>
2018-11-30 15:20:45 +0800

    略

--- xuetuzhi/esm/InfoTabLayout/index.js
+++ xuetuzhi/esm/InfoTabLayout/index.js
@@ -1,39 +1,35 @@
-import React from 'react'
-import { View, Text } from 'react-native'
-import LinearGradient from 'react-native-linear-gradient';
-import { fontSizeXs, row, spaceBtw, mt_xs, bgPrimary, colorWhite, getFontWeight, mt_sm, fontSizeSm, getFlex } from '../../../styles';
-
-import { StatusBadge, PaddingBox } from '../index';
-export default class InfoBlock extends React.Component {
+import React, { Component } from 'react'
+import { Tab, Tabs, View, Text, } from 'native-base'
+import { pp_sm, pv_n, px2dp, getFlex, bgPrimary, bgBlack, colorWhite } from '../../../styles';
+import { default as InfoBlock } from '../InfoBlock';
+export default class InfoTabLayout extends Component {
     constructor() {
-        super(...arguments);
-
+        super(...arguments)
     }
+
+
     render() {
-        const { rows = [], style = [], onPress, bg = bgPrimary, bgColors = ['#66ccff', '#3399ff', '#0066cc'], color = colorWhite, textStyle = [] } = this.props;
-        const _textStyle = [color, fontSizeSm, ...textStyle]
+        const { contentTabs, rows, bg = bgPrimary, color = colorWhite,textStyle } = this.props
         return (
-            <LinearGradient colors={bgColors}  start={{ x: 0, y: 0 }} end={{ x: 1, y: 0 }}>
-                <PaddingBox style={[...style]} onPress={onPress}>
+            <View style={[getFlex()]}>
+                <InfoBlock rows={rows} bg={bg} color={color} textStyle={textStyle} />
+                <Tabs initialPage={0} tabBarUnderlineStyle={{ height: px2dp(1.5) }}>
                     {
-                        rows.map((r, index) => {
+                        Object.entries(contentTabs).map((i, index) => {
+                            const key = i[0]
+                            const value = i[1]
                             return (
-                                <View style={[row, spaceBtw, (index !== 0) && mt_sm]}>
+                                <Tab heading={key} key={index}>
                                     {
-                                        r.map(rowItem => {
-                                            return <Text style={_textStyle}>{rowItem}</Text>
-                                        })
+                                        value
                                     }
-                                </View>
+                                </Tab>
                             )
                         })
                     }
-                </PaddingBox>
-            </LinearGradient>
+                </Tabs>
+
+            </View>
         )
     }
-
-
 }
-
-
