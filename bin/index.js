1914f978f4c1b6b61e00ba7cb6de5f93208953d1
帆 <lee@fandeMacBook-Air.local>
2018-12-26 16:43:33 +0800

    shelljs

--- bin/index.js
+++ bin/index.js
@@ -1,20 +1,42 @@
 #!/usr/bin/env node
+var THEME_KEY = 'theme'
+var FONTS_KEY = 'fonts-path'
+
 var path = require('path')
 var fs = require('fs')
+var cwd = process.cwd()
+var repositoryPath = require.resolve('zhilin-rn')
 
-var TAGET = 'theme'
-// var root = path.resolve(__dirname, '../../')
+var pkg = JSON.parse(fs.readFileSync(resolve('package.json')).toString('utf8'))
 
-var pkg = fs.readFileSync(path.resolve('package.json')).toString('utf8')
-var pkgThemePath = JSON.parse(pkg)[TAGET]
-var customThemePath = path.resolve(pkgThemePath)
+var customThemePath = resolve(pkg[THEME_KEY])
+var fontsPath = resolve(pkg[FONTS_KEY])
 
+//theme
 var customTheme = require(customThemePath) || {}
-var themePath = path.resolve(require.resolve('zhilin-rn'), '../../theme.js')
-// var theme = require(themePath)
+var themePath = path.resolve(repositoryPath, '../../theme.js')
 var theme = {}
 var result = Object.assign({}, theme, customTheme)
 if (fs.existsSync(themePath)) {
     fs.writeFileSync(themePath, `export default ${JSON.stringify(result)}`)
 }
-console.log('_________',__dirname)
+
+//iconfont
+var generateIconSetFromCss = require('react-native-vector-icons/lib/generate-icon-set-from-css');
+var iconfontCss = path.resolve(fontsPath, 'iconfont.css')
+var iconfontJson = resolveRepository('icons/iconfont.json')
+var sourceTtf = path.resolve(fontsPath, 'iconfont.ttf')
+var targetTtf = path.resolve('node_modules/react-native-vector-icons/Fonts/iconfont.ttf')
+
+var iconSet = generateIconSetFromCss(iconfontCss, 'icon-')
+fs.writeFileSync(iconfontJson, iconSet)
+fs.copyFileSync(sourceTtf, targetTtf)
+
+console.log('______', targetTtf)
+
+function resolve(p) {
+    return path.resolve(cwd, p)
+}
+function resolveRepository(p) {
+    return path.resolve(repositoryPath, '../..', p)
+}
