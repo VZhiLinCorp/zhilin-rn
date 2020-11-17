36a1ebe526d7e82897aca2f1fb34c7005a28e6df
VZhiLinCorp <35518845+VZhiLinCorp@users.noreply.github.com>
2020-11-17 14:05:32 +0800

    fix isIphoneX

--- utils/index.d.ts
+++ utils/index.d.ts
@@ -6,7 +6,7 @@ export const screenW = Dimensions.get("window").width;
 export const screenH = Dimensions.get("window").height;
 
 export const isAndroid = Platform.OS === 'android'
-export const isIphoneX = Platform.OS === "ios" && (screenH === 812 || screenW === 812);
+export const isIphoneX = Platform.OS === "ios" && (screenH === 812 || screenW === 812 || screenH===896||screenW===896);
 
 export { default as autobind } from 'autobind-decorator'
 export { default as emitter } from './emitter'
