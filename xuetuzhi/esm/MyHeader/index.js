e29253498200e558da6991c502b935fe5fdab38f
VZhiLinCorp <35518845+VZhiLinCorp@users.noreply.github.com>
2022-08-29 17:41:04 +0800

    MyHeader背景及字体颜色修改

--- xuetuzhi/esm/MyHeader/index.js
+++ xuetuzhi/esm/MyHeader/index.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import { View, Text, Header, Button, Icon, Title } from 'native-base'
 import { NavigationActions, withNavigation } from 'react-navigation'
-import { bgWhite, colorPrimary, center, px2dp, getBorder, headerHeight, getFlex, alignItemsC, COLOR_INFO, fontSizeMd, COLOR_BLACK, row, bgDanger, jc_c, spaceBtw, flexEnd, p_0, pl_xs } from 'zhilin-rn/styles';
+import {StatusBar} from "react-native";
+import { bgWhite, colorPrimary, center, px2dp, getBorder, headerHeight, getFlex, alignItemsC, COLOR_INFO, fontSizeMd, COLOR_WHITE, row, bgDanger, jc_c, spaceBtw, flexEnd, p_0, pl_xs } from 'zhilin-rn/styles';
 import { isAndroid } from '../../../utils';
 import { m_0 } from '../../../styles';
 import { headerPadding } from '../../../styles/common';
@@ -19,13 +20,13 @@ export default class MyHeader extends React.Component {
     render() {
         const { showLeft = true, renderRight, route, leftText, bgColor, color, renderBody, ChevronLeft, nb, onPress } = this.props
         return (
-           <Header style={[{ marginTop: -1, paddingTop: px2dp(24) }, spaceBtw, alignItemsC, nb ? { borderBottomWidth: 0 } : getBorder('b'), headerHeight, headerPadding, { backgroundColor: bgColor ? bgColor : "#F8F8F8" }]}>
+           <Header style={[{ marginTop: -1, paddingTop: px2dp(24) }, spaceBtw, alignItemsC, nb ? { borderBottomWidth: 0 } : getBorder('b'), headerHeight, headerPadding, { backgroundColor: bgColor ? bgColor : "#2F54EB" }]}>
                 <View style={[getFlex(3), row, alignItemsC]}>{
                     (route || showLeft) &&
                     <Button transparent style={[getFlex(1), { marginLeft: isAndroid ? px2dp(-5) : 0, paddingLeft: 0 }]} onPress={onPress ? onPress : this._nav} dark>
                         {
-                            leftText ? <Text style={[colorPrimary,pl_xs, { color: color ? color : COLOR_BLACK }]}>{leftText}</Text>
-                                : <Icon type="Entypo" name="chevron-thin-left" style={[m_0, { color: color ? color : COLOR_BLACK }]} />
+                            leftText ? <Text style={[colorPrimary,pl_xs, { color: color ? color : COLOR_WHITE }]}>{leftText}</Text>
+                                : <Icon type="Entypo" name="chevron-thin-left" style={[m_0, { color: color ? color : COLOR_WHITE }]} />
                         }
 
                     </Button>
@@ -33,7 +34,7 @@ export default class MyHeader extends React.Component {
                 }</View>
                 <View style={[getFlex(10), row, jc_c]}>
                     {
-                        renderBody ? renderBody() : <Title style={[fontSizeMd, { color: color ? color : COLOR_BLACK }]} >{this.props.title}</Title>
+                        renderBody ? renderBody() : <Title style={[fontSizeMd, { color: color ? color : COLOR_WHITE }]} >{this.props.title}</Title>
                     }
                 </View>
                 <View style={[getFlex(3), flexEnd, row, alignItemsC]}>
@@ -42,7 +43,7 @@ export default class MyHeader extends React.Component {
                         renderRight()
                     }
                 </View>
-
+                <StatusBar barStyle={'light-content'} backgroundColor={'transparent'} translucent={true} hidden={false} />
             </Header>
         )
     }
